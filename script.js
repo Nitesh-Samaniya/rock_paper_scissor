e72f337aebf8f21f7668b351fa4025d833650a75
Nitesh-Samaiya <nitinsamaniya97@gmail.com>
2023-03-13 13:43:54 +0530

    user choosen symbol completed

--- script.js
+++ script.js
@@ -7,6 +7,28 @@ let title_with_score = document.querySelector(".title_with_score");
 let img_for_rule = false;
 let img = document.querySelector(".rules_and_next-IMG");
 
+let user_choosen_value = "";
+let symbol_div = document.querySelector(".symbols");
+
+let paper_symbol = document.querySelector("#paper");
+paper_symbol.addEventListener("click", function(){
+    user_choosen_value = "paper";
+    symbol_div.style.display = "none";
+    // alert(user_choosen_value);
+})
+
+let rock_symbol = document.querySelector("#rock");
+rock_symbol.addEventListener("click", function(){
+    user_choosen_value = "rock";
+    symbol_div.style.display = "none";
+})
+
+let scissor_symbol = document.querySelector("#scissor");
+scissor_symbol.addEventListener("click", function(){
+    user_choosen_value = "scissor";
+    symbol_div.style.display = "none";
+})
+
 function theme(){
     if(themeCode){
         themeCode = false;
@@ -31,4 +53,8 @@ function gameRules(){
         img.style.display = "none"
         img_for_rule = false;
     }
+}
+
+function nextBtn(){
+    symbol_div.style.display = "block";
 }
