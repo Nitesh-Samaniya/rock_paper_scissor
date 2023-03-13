14bbab6837660f765a2cf5dc570d9024190d0bde
Nitesh-Samaiya <nitinsamaniya97@gmail.com>
2023-03-13 12:21:38 +0530

    game rules added

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let body = document.querySelector("body");
 let themeBtn = document.querySelector(".themeBtn");
 let title_with_score = document.querySelector(".title_with_score");
 
+
+let img_for_rule = false;
+let img = document.querySelector(".rules_and_next-IMG");
+
 function theme(){
     if(themeCode){
         themeCode = false;
@@ -16,5 +20,15 @@ function theme(){
         body.style.color = "white"
         themeBtn.textContent = "Light Theme"
         title_with_score.style.border = "1px solid white"
+    }
+}
+
+function gameRules(){
+    if(!img_for_rule){
+        img.style.display = "block"
+        img_for_rule = true;
+    }else{
+        img.style.display = "none"
+        img_for_rule = false;
     }
 }
